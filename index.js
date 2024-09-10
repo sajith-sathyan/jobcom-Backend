426b624acdc61b17d8843f3f7cf6480af6b661bf
sajith-sathyan <113768183+sajith-sathyan@users.noreply.github.com>
2024-09-10 20:55:24 +0530

    add cors of vercel

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const { Server } = require("socket.io");
 const cors = require("cors");
 const app = express();
 const mongoose = require("mongoose");
-const dbConnection = require('./config/dataBaseConfig')
+const dbConnection = require('./config/dataBaseConfig');
 const userRoute = require("./routes/userRoutes");
-const adminRoute = require("./routes/adminRoutes")
+const adminRoute = require("./routes/adminRoutes");
 const socketHandler = require("./socketConnection/socketConnection");
 
 require("dotenv").config();
 
 const server = app.listen(8000, "localhost", () => {
-  console.log("Server is running on http://localhost:8080");
+  console.log("Server is running on http://localhost:8000");
 });
 
 const io = new Server(server, {
@@ -21,22 +21,35 @@ const io = new Server(server, {
 // Call the socketHandler function and pass the `io` object
 socketHandler(io);
 
-
-const corsOptions = {
-  origin: "*",
-  credentials: true,
+// Define a CORS Middleware Function
+const allowCors = (req, res, next) => {
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
+  
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+  
+  next();
 };
-app.use(cors(corsOptions));
 
-app.use(cors(corsOptions));
+// Use the CORS Middleware
+app.use(allowCors);
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Welcome to the Jobcom website! ");
+  res.send("Welcome to the Jobcom website!");
 });
+
 app.use("/api/user-route", userRoute);
 app.use("/api/admin-route", adminRoute);
 
 // Disable strictQuery option
-dbConnection
+dbConnection;
