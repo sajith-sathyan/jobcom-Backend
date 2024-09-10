6930cfc10ac1e3661abdf60f7ac5af70a4886930
sajith-sathyan <113768183+sajith-sathyan@users.noreply.github.com>
2024-09-10 21:24:57 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,56 +1,28 @@
 const express = require("express");
-const { Server } = require("socket.io");
-const cors = require("cors");
-const app = express();
-const mongoose = require("mongoose");
-const dbConnection = require('./config/dataBaseConfig');
-const userRoute = require("./routes/userRoutes");
-const adminRoute = require("./routes/adminRoutes");
-const socketHandler = require("./socketConnection/socketConnection");
+const userRoute = express.Router();
+const {
+  register,
+  login,
+  sendDataToInterviewerLiveDatabase,
+  sendDataToCandidateLiveDatabase,
+  jobSearchNaukri,
+  jobSearchLinkedin,
+  createCheckoutSession,
+  createResume,
+  getUser,
+  verifyPassword
+} = require("../controllers/userController");
 
-require("dotenv").config();
+// Define your user routes here    job-search job-search-linkedin get-user
+userRoute.post("/register", register);
+userRoute.post("/login", login);
+userRoute.get("/get-user", getUser);
+userRoute.post("/verify-password",  verifyPassword);
+userRoute.post("/send-data-to-interviewerLive-database",sendDataToInterviewerLiveDatabase);
+userRoute.post("/send-data-to-candidateLive-database",sendDataToCandidateLiveDatabase);
+userRoute.post("/job-search-naukri",jobSearchNaukri);
+userRoute.post("/job-search-linkedin",jobSearchLinkedin);
+userRoute.post("/create-checkout-session",createCheckoutSession);
+userRoute.post("/create-resume",createResume);
 
-// Socket.io configuration with CORS
-const server = app.listen(8000, "localhost", () => {
-  console.log("Server is running on http://localhost:8000");
-});
-
-const io = new Server(server, {
-  cors: {
-    origin: ['http://localhost:3000', 'https://jobcom.website', 'https://jobcom-backend.vercel.app'],
-    credentials: true,
-  },
-});
-
-// Call the socketHandler function and pass the `io` object
-socketHandler(io);
-
-// Define CORS options for Express
-const corsOptions = {
-  origin: (origin, callback) => {
-    const allowedOrigins = ['http://localhost:3000', 'https://jobcom.website', 'https://jobcom-backend.vercel.app'];
-    if (allowedOrigins.includes(origin) || !origin) {
-      callback(null, true); // Allow the request if the origin is in the list or if there's no origin (non-browser requests)
-    } else {
-      callback(new Error('Not allowed by CORS')); // Reject the request otherwise
-    }
-  },
-  credentials: true, // Allow credentials (cookies, authorization headers)
-};
-
-// Enable CORS middleware
-app.use(cors(corsOptions));
-
-// Express JSON middleware
-app.use(express.json());
-
-// Define routes
-app.get("/", (req, res) => {
-  res.send("Welcome to the Jobcom website! Everything is working well.");
-});
-
-app.use("/api/user-route", userRoute);
-app.use("/api/admin-route", adminRoute);
-
-// Disable strictQuery option for Mongoose
-dbConnection;
+module.exports = userRoute;
